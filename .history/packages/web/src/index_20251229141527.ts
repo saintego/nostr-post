731b7667dfc26ff5db2b1c5452e2f6739136ac60
stego <stegena@gmail.com>
2025-12-29 14:49:21 +0100

    remove history

--- .history/packages/web/src/index_20251229141527.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @nostr-post/web - Web Components entry point
- *
- * Universal, framework-independent UI for nostr-post
- */
-
-export { NostrPostComposer } from './composer';
-export { NostrPostView } from './view';
-export { NostrPostElement } from './base-component';
