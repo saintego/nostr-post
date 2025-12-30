9c2df46b3fbaeecc22c6b5657eafae4c7d3c2446
stego <stegena@gmail.com>
2025-12-30 18:51:25 +0100

    fix: add null check for root element before rendering the app

--- examples/react-demo/src/main.tsx
+++ examples/react-demo/src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
     <StrictMode>
-        <App />
+      <App />
     </StrictMode>
-);
+  );
+}
